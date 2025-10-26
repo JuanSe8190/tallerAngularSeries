0492744f3c156399c99911d7f3db6c71b3e7295e
Juancho-bas <js.avendanor1@uniandes.edu.co>
2025-10-26 16:37:32 -0500

    avance taller Angular

--- src/app/serie/serie-list/serie-list.ts
+++ src/app/serie/serie-list/serie-list.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Serie} from '../serie'
+import { SerieService } from '../serie.service';
+
 
 @Component({
   selector: 'app-serie-list',
@@ -7,7 +9,22 @@ import {Serie} from '../serie'
   templateUrl: './serie-list.html',
   styleUrl: './serie-list.css',
 })
-export class SerieList {
-  series: Array<Serie>=[];
+export class SerieList implements OnInit{
+  series: Array<Serie> = [];
+
+  constructor(private serieService: SerieService) {}
+
+  getSeriesList(){
+    this.serieService.getSeries().subscribe((series)=>{
+      this.series=series;
+    });
+  }
+
+  ngOnInit() {
+    this.getSeriesList();
+      
+  }
+
+
 
 }

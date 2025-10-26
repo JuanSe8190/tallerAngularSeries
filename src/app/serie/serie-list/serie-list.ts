f617e7fbbe79ffa33c38a4f5d6e293202a3152fb
Juancho-bas <js.avendanor1@uniandes.edu.co>
2025-10-26 17:38:15 -0500

    final taller

--- src/app/serie/serie-list/serie-list.ts
+++ src/app/serie/serie-list/serie-list.ts
@@ -11,18 +11,35 @@ import { SerieService } from '../serie.service';
 })
 export class SerieList implements OnInit{
   series: Array<Serie> = [];
+  averageSeasons = 0;
+  selected: Serie | null = null; 
 
   constructor(private serieService: SerieService) {}
 
   getSeriesList(){
     this.serieService.getSeries().subscribe((series)=>{
       this.series=series;
+      this.averageSeasons = this.calcularPromedioTemporadas(this.series);
     });
+  }
+
+  onSelect(s: Serie) {              
+    this.selected = s;
   }
 
   ngOnInit() {
     this.getSeriesList();
+    
       
+  }
+
+  private calcularPromedioTemporadas(list: Serie[]): number {
+    let total = 0, count = 0;
+    for (const s of list) {
+      total += s.seasons;
+      count++;
+    }
+    return count ? total / count : 0;
   }
 
 
